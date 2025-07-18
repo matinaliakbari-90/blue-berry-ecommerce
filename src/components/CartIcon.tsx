6ea73b2ccaa1111e016bdd2330f0fcd8e7db6e5a
matin aliakbari <matin54843590@gmail.com>
2025-07-19 01:02:03 +0330

    Changed in file Header

---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,16 @@
+import { ShoppingBag } from "lucide-react";
+import Link from "next/link";
+
+export default function CartIcon() {
+    return (
+        <Link href="/cart" className="flex items-center text-sm gap-2 border border-gray-200 px-2 py-1 rounded-md shadow-md hover:shadow-none hoverEffect">
+            <ShoppingBag className="text-darkBlue w-6 h-6" />
+            <div className="flex flex-col">
+                <p className="text-xs">
+                    <span className="font-semibold">0</span> items
+                </p>
+                <p className="font-semibold">Cart</p>
+            </div>
+        </Link>
+    );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import logo from '../../public/images/logo.png';
 import Form from "next/form";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
+import { ShoppingBasket } from "lucide-react";
 
 export default function Header() {
     return (
@@ -13,8 +15,20 @@ export default function Header() {
                 <Form action='search' className="flex-1">
                     <input type="text" name="query" className="w-full border-2 border-gray-200 px-4 py-2.5 rounded-md focus-visible:border-darkBlue outline-0" placeholder="Search for products ..." />
                 </Form>
-                <div>
-                    
+                <div className="flex items-center gap-5">
+                    <CartIcon />
+
+                    <Link href="/orders" className="flex items-center text-sm gap-2 border border-gray-200 px-2 py-1 rounded-md shadow-md hover:shadow-none hoverEffect">
+                        <ShoppingBasket className="text-darkBlue w-6 h-6" />
+                        <div className="flex flex-col">
+                            <p className="text-xs">
+                                <span className="font-semibold">0</span> items
+                            </p>
+                            <p className="font-semibold">Orders</p>
+                        </div>
+                    </Link>
+
+                    <p>Account</p>
                 </div>
             </div>
         </header>
