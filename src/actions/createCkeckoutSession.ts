c021dd194f2d42f762f2b06543448d0e5ae3c07d
matin aliakbari <matin54843590@gmail.com>
2025-07-31 15:04:29 +0330

    Edditing for action

--- src/actions/createCkeckoutSession.ts
+++ src/actions/createCkeckoutSession.ts
@@ -43,8 +43,8 @@ export async function createCheckoutSession(items: GroupedCartItems[], metadata:
             mode: "payment",
             allow_promotion_codes: true,
             payment_method_types: ["card"],
-            success_url: `${process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-            cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}`}/cart`,
+            success_url: `https://blue-berry-ecommerce.vercel.app/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
+            cancel_url: `https://blue-berry-ecommerce.vercel.app/cart`,
             line_items: items.map((item) => ({
                 price_data: {
                     currency: "USD",
